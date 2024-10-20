69123164f74aa347cc253e2c8f83ef9c93006304
Daniela C. Barbosa <contato@danielacb.com>
2024-10-20 00:51:33 -0300

    update old portfolio urls

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/2015',
+        destination: '/2015/index.html',
+        permanent: true,
+      },
+      {
+        source: '/2016',
+        destination: '/2016/index.html',
+        permanent: true,
+      },
+      {
+        source: '/2017',
+        destination: '/2017/index.html',
+        permanent: true,
+      },
+      {
+        source: '/2023',
+        destination: '/2023/index.html',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
