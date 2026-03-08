06f582d0e40566e4b5878fff75b0b83a6571a246
Daniela C. Barbosa <contato@danielacb.com>
2026-03-07 22:32:30 -0300

    reset project and migrate to vite

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Daniela Barbosa</h1>
+      <p className="text-2xl font-bold">Front-end Engineer</p>
+    </div>
+  );
+}
+
+export default App;
